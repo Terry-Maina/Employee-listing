29cd2292e77baade5de8dc1e7b80f9b89734d8ac
Teresa <tenjerim@gmail.com>
2023-01-28 21:14:34 +0300

    emp detail

---
+++ src/EmpDetail.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+
+const EmpDetail = () => {
+    const { empid } = useParams();
+
+    const [empdata, empdatachange] = useState({});
+
+    useEffect(() => {
+        fetch("http://localhost:3000/employee/" + empid).then((res) => {
+            return res.json();
+        }).then((resp) => {
+            empdatachange(resp);
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }, []);
