e2d29f093088c0e8991121998743d5d8900f9ddc
Teresa <tenjerim@gmail.com>
2023-01-28 21:38:13 +0300

    handle submit

---
+++ src/EmpCreate.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const EmpCreate = () => {
+
+    const[id,idchange]=useState("");
+    const[name,namechange]=useState("");
+    const[email,emailchange]=useState("");
+    const[phone,phonechange]=useState("");
+    const[active,activechange]=useState(true);
+    const[validation,valchange]=useState(false);
+
+
+    const navigate=useNavigate();
+
+    const handlesubmit=(e)=>{
+      e.preventDefault();
+      const empdata={name,email,phone,active};
+      
+
+      fetch("http://localhost:8000/employee",{
+        method:"POST",
+        headers:{"content-type":"application/json"},
+        body:JSON.stringify(empdata)
+      }).then((res)=>{
+        alert('Saved successfully.')
+        navigate('/');
+      }).catch((err)=>{
+        console.log(err.message)
+      })
+
+    }
+
+    return (
+        <div>
+
+            <div className="row">
+                <div className="offset-lg-3 col-lg-6">
+                    <form className="container" onSubmit={handlesubmit}>
+
+                        <div className="card" style={{"textAlign":"left"}}>
+                            <div className="card-title">
+                                <h2>Employee Create</h2>
+                            </div>
