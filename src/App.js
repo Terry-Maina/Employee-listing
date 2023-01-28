21af7b5b2f5716d84a91fba8e1ca995535090f42
Teresa <tenjerim@gmail.com>
2023-01-28 20:19:27 +0300

    route

--- src/App.js
+++ src/App.js
@@ -5,3 +5,12 @@ import EmpListing from './EmpListing';
 import EmpCreate from './EmpCreate';
 import EmpDetail from './EmpDetail';
 import EmpEdit from './EmpEdit';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Employee List</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<EmpListing />}></Route>
+          <Route path='/employee/create' element={<EmpCreate />}></Route>
