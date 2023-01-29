2d16dce94df2f75b705191422c48119be29cb33a
Teresa <tenjerim@gmail.com>
2023-01-29 08:39:13 +0300

    completed

--- src/EmpListing.js
+++ src/EmpListing.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+
 const EmpListing = () => {
     const [empdata, empdatachange] = useState(null);
     const navigate = useNavigate();
