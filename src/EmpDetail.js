01248b700eba31aaaa235326c15e115d87c2944e
Teresa <tenjerim@gmail.com>
2023-01-29 08:00:57 +0300

    empdetail

--- src/EmpDetail.js
+++ src/EmpDetail.js
@@ -16,3 +16,34 @@ const EmpDetail = () => {
             console.log(err.message);
         })
     }, []);
+    return (
+        <div>
+            {/* <div className="row">
+                <div className="offset-lg-3 col-lg-6"> */}
+
+               <div className="container">
+                
+            <div className="card row" style={{ "textAlign": "left" }}>
+                <div className="card-title">
+                    <h2>Employee Create</h2>
+                </div>
+                <div className="card-body"></div>
+
+                {empdata &&
+                    <div>
+                        <h2>The Employee name is : <b>{empdata.name}</b>  ({empdata.id})</h2>
+                        <h3>Contact Details</h3>
+                        <h5>Email is : {empdata.email}</h5>
+                        <h5>Phone is : {empdata.phone}</h5>
+                        <Link className="btn btn-danger" to="/">Back to Listing</Link>
+                    </div>
+                }
+            </div>
+            </div>
+            {/* </div>
+            </div> */}
+        </div >
+    );
+}
+
+export default EmpDetail;
